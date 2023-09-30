cba0dd3731b26c032c49b91eb82aa23afc48f494
Влада Мухатдинова <ladamuxa33@gmail.com>
2023-09-30 20:30:15 +0300

    added atrrs to follow model

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -45,6 +45,35 @@ class User(AbstractUser):
         verbose_name = _('user')
         verbose_name_plural = _('users')
 
+    def __str__(self):
+        return self.email
+
 
 class Follow(models.Model):
-    pass
+    
+    follower = models.ForeignKey(
+        User,
+        related_name='follower',
+        on_delete=models.CASCADE,
+        verbose_name=_('follower')
+    )
+
+    author = models.ForeignKey(
+        User,
+        related_name='author',
+        on_delete=models.CASCADE,
+        verbose_name=_('author')
+    )
+
+    class Meta:
+        verbose_name = _('subscription')
+        verbose_name_plural = _('subscriptions')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['follower', 'author'],
+                name='unique_subscription'
+            )
+        ]
+    
+    def __str__(self):
+        return _(f'{self.follower} subscribed to: {self.author}')
