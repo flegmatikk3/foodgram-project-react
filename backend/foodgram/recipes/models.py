ba3082ef459aa4676cb0a062c6ede66ae3d51523
Влада Мухатдинова <ladamuxa33@gmail.com>
2023-10-01 16:52:21 +0300

    some changes to models

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -7,9 +8,9 @@ User = get_user_model()
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    color = models.CharField(max_length=7, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(_('title') ,max_length=200, unique=True)
+    color = models.CharField(_('color'), max_length=7, unique=True)
+    slug = models.SlugField(_('slug of tag'), max_length=200, unique=True)
 
     class Meta:
         verbose_name = _('Tag')
@@ -20,8 +21,8 @@ class Tag(models.Model):
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=200)
-    measurement_unit = models.CharField(max_length=200)
+    name = models.CharField(_('title') ,max_length=200)
+    measurement_unit = models.CharField(_('measurement unit'), max_length=200)
 
     class Meta:
         verbose_name = _('Ingredient')
@@ -32,18 +33,35 @@ class Ingredient(models.Model):
 
 
 class Recipe(models.Model):
-    tags = models.ManyToManyField(Tag)
-    name = models.CharField(max_length=200)
-    cooking_time = models.PositiveIntegerField() # dobavit validation na min znach
-    text = models.TextField()
-    image =
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    ingredients = models.ManyToManyField(
-        'Ingredient',
-        through='RecipeIngredient',
-        through_fields=('recipe', 'ingredient')
+    tags = models.ManyToManyField(Tag, verbose_name=_('tags'))
+    name = models.CharField(_('title'), max_length=200)
+
+    cooking_time = models.PositiveIntegerField(
+        _('cookings time in minutes'),
+        validators=[
+            MinValueValidator(
+                1, message=_('minimal time to cook is 1 minute')
+            )
+        ]
     )
-    pub_date = 
+
+    text = models.TextField(_('description'))
+    image = models.ImageField(_('image'), upload_to='media/')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=_('author')
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        through_fields=('recipe', 'ingredient'),
+        verbose_name=_('ingredients')
+    )
+    pub_date = models.DateTimeField(
+        _('publication date'),
+        auto_now_add=True,
+    )
 
     class Meta:
         verbose_name = _('Recipe')
@@ -54,9 +72,32 @@ class Recipe(models.Model):
 
 
 class RecipeIngredient(models.Model):
-    recipe = models.ForeignKey(Recipe, related_name='recipe_ingredients', on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    amount = models.PositiveIntegerField()
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='recipe_ingredients',
+        on_delete=models.CASCADE
+        verbose_name=_('recipe')
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name=_('ingredient')
+    )
+    amount = models.PositiveIntegerField(
+        _('amount'),
+        validators = [
+            MinValueValidator(
+                1, message=_('minimal amount of ingredients 1')
+            )
+        ]
+    )
+
+    class Meta:
+        verbose_name = _('ingredient in recipe')
+        verbose_name_plural = _('ingredients in recipe')
+
+    def __str__(self):
+        return f'{self.ingredient} {self.amount}'
 
 
 class Favorite(models.Model):
