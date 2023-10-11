8689b07bfcb29fd92f6f34d4e3199cea0260e117
Влада Мухатдинова <ladamuxa33@gmail.com>
2023-10-12 00:53:03 +0300

    i broke up

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,9 +5,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-DEBUG = os.getenv('DEBUG', default=False)
+debug = os.getenv('DEBUG', 'False')
+DEBUG = (debug == 'True')
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.78.165', 'foodgram-fuck-your-mum.ru']
 
 AUTH_USER_MODEL = 'users.User'
 
